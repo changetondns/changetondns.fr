98af8a04abcfb1ba7aff0cb9803f5150d6919010
Jourdelune <jourdelune863@gmail.com>
2023-05-14 11:50:32 +0200

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Tutorial from '../components/Tutorial.vue'
+</script>
+
+<template>
+  <main class="font-['inter']">
+    <section class="w-screen">
+      
+      <!-- Titre -->
+      <div class="flex flex-wrap w-fit mx-auto">
+        <p class="text-[#345BC1] text-[64px] font-semibold mr-3 text-center">Change ton</p>
+        <div class="relative inline-block w-fit mx-auto">
+          <p class="text-[#345BC1] text-[64px] font-semibold z-10 relative">DNS</p>
+          <div class="absolute transform -rotate-6 -translate-y-full bg-[#FF9696] w-36 h-20 z-5"></div>
+        </div>
+      </div>
+
+  
+    </section>
+
+    <Tutorial />
+  </main>
+</template>
