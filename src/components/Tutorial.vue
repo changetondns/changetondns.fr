887af21674a392a954c893a5d22f878947f58e7e
Jourdelune <jourdelune863@gmail.com>
2023-05-15 20:30:26 +0200

    ajoute d'option

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -1,8 +1,19 @@
 <script setup>
 </script>
 
+<script>
+export default {
+  props: ['plateforme', 'theme']
+}
+</script>
+
 <template>
   <div class="h-96">
-
+    <section v-if="plateforme == 'w-11'">
+        <p class="text-[#1C1C1C] font-bold text-xl" :class="{ 'text-[#DBDBDB]': !theme }">Changer de DNS sur Windows 11.</p>
+    </section>
+    <section v-else>
+        <p>lol</p>
+    </section>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,38 +12,17 @@ import shema_white from '../assets/shema_white.svg'
 import axios from 'axios'
 
 export default {
-    data() {
-        return {
-            moon,
-            sun,
-            github,
-            twitter,
-            shema_white,
-            shema_black,
-            light_theme: true,
-        }
-    },
-    methods: {
-        dns() {
-            // TODO generate random value for the ids
-
-            const id1 = 7890001478371854
-            const id2 = 5798210465243758
-            axios.get('http://' + id1 + "" + id2 + '.edns.ip-api.com/json').then((res) => {
-                console.log(res.data)
-            }).catch((err) => {
-                console.log(err)
-            })
-            // change to the client ip
-            axios.get("http://ip-api.com/json/92.184.96.99?fields=62464").then((res) => {
-                console.log(res.data)
-            }).catch((err) => {
-                console.log(err)
-            })
-        }
-
+  data() {
+    return {
+      moon,
+      sun,
+      github,
+      twitter,
+      shema_white,
+      shema_black,
+      light_theme: true,
     }
-
+  },
 }
 </script>
 
@@ -54,38 +33,33 @@ export default {
             <img class="w-8 h-8" :src="sun" v-else/>
         </div>
 
-        <section class="mx-4 h-auto md:h-screen min-h-[800px] relative grid grid-cols-1 gap-1">
-            <!-- Titre -->
-            <div>
-                <div class="flex flex-wrap w-fit max-w-sm sm:max-w-full m-auto mt-24">
-                    <p class="text-[#345BC1] text-[64px] font-semibold mx-auto sm:mx-3 text-center">Change ton</p>
-                    <div class="relative inline-block w-fit mx-auto">
-                        <p class="text-[#345BC1] text-[64px] font-semibold z-10 relative">DNS</p>
-                        <div class="absolute transform -rotate-6 -translate-y-full bg-[#FF9696] w-36 h-20 z-5"></div>
-                    </div>
-                </div>
+    <section class="mx-4 h-auto md:h-screen min-h-[800px] relative grid grid-cols-1 gap-1">
+      <!-- Titre -->
+      <div>
+        <div class="flex flex-wrap w-fit max-w-sm sm:max-w-full m-auto mt-24">
+          <p class="text-[#345BC1] text-[64px] font-semibold mx-auto sm:mx-3 text-center">Change ton</p>
+          <div class="relative inline-block w-fit mx-auto">
+            <p class="text-[#345BC1] text-[64px] font-semibold z-10 relative">DNS</p>
+            <div class="absolute transform -rotate-6 -translate-y-full bg-[#FF9696] w-36 h-20 z-5"></div>
+          </div>
+        </div>
+        
+        <p class="text-[#5E5E5E] text-lg text-center mt-2" :class="{ 'text-[#F9F9F9]': !light_theme }">Évitons la censure !</p>
+        
+      </div>
+      
+      <!-- DNS check -->
+      <div>
+        <div class="w-fit mx-auto mt-14">
+          <button type="button" class="text-white bg-[#345BC1] rounded px-4 py-3 text-2xl" style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">
+          Cliquez içi pour vérifier votre DNS
+          </button>
+        </div>
 
-                <p class="text-[#5E5E5E] text-lg text-center mt-2" :class="{ 'text-[#F9F9F9]': !light_theme }">Évitons
-                    la censure !</p>
-
-            </div>
-
-            <!-- DNS check -->
-
-            <div>
-                <div class="w-fit mx-auto mt-14">
-                    <button type="button" class="text-white bg-[#345BC1] rounded px-4 py-3 text-2xl"
-                            style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);" @click="dns">
-                        Cliquez içi pour vérifier votre DNS
-                    </button>
-                </div>
-
-                <p class="text-[#5E5E5E] text-center text-2xl max-w-2xl mx-auto mt-8"
-                   :class="{ 'text-[#F9F9F9]': !light_theme }">
-                    En cliquant sur le bouton, vous pourrez vérifier votre DNS et savoir si vous êtes soumis à la
-                    censure d'internet.
-                </p>
-            </div>
+        <p class="text-[#5E5E5E] text-center text-2xl max-w-2xl mx-auto mt-8" :class="{ 'text-[#F9F9F9]': !light_theme }">
+          En cliquant sur le bouton, vous pourrez vérifier votre DNS et savoir si vous êtes soumis à la censure d'internet.
+        </p>
+      </div>
 
             <!-- bulle d'info-->
             <div class="max-w-3xl bg-[#2E2D39] p-4 rounded-lg mx-auto relative h-fit mt-12"
@@ -100,28 +74,24 @@ export default {
 
         </section>
 
-        <section class="mx-auto w-3/4 md:-translate-y-5 mt-12 md:mt-0">
-            <!-- selection de la plateforme-->
-            <div class="block md:flex">
-                <p class="mr-3 text-xl text-[#2E2E2E] my-auto" :class="{ 'text-[#FFFFFF]': !light_theme }">Je veux
-                    changer de DNS sur :</p>
-                <div class="border-solid border-2 border-[#8B8B8B] px-2 py-1 rounded-md w-fit"
-                     :class="{ 'border-[#CFCFCF]': !light_theme }">
-                    <select class="text-[#8B8B8B] text-xl outline-none" id="plateform-select"
-                            :class="{ 'text-[#CFCFCF] bg-[#161818]': !light_theme }">
-                        <option value="w-11">Windows 11</option>
-                        <option value="w-10">Windows 10</option>
-                        <option value="mac">Mac</option>
-                        <option value="android">Android</option>
-                        <option value="ios">IOS</option>
-                    </select>
-                </div>
-            </div>
+    <section class="mx-auto w-3/4 md:-translate-y-5 mt-12 md:mt-0">
+      <!-- selection de la plateforme-->
+      <div class="block md:flex">
+        <p class="mr-3 text-xl text-[#2E2E2E] my-auto" :class="{ 'text-[#FFFFFF]': !light_theme }">Je veux changer de DNS sur :</p>
+        <div class="border-solid border-2 border-[#8B8B8B] px-2 py-1 rounded-md w-fit" :class="{ 'border-[#CFCFCF]': !light_theme }">
+          <select class="text-[#8B8B8B] text-xl outline-none" id="plateform-select" :class="{ 'text-[#CFCFCF] bg-[#161818]': !light_theme }">
+            <option value="w-11">Windows 11</option>
+            <option value="w-10">Windows 10</option>
+            <option value="mac">Mac</option>
+            <option value="android">Android</option>
+            <option value="ios">IOS</option>
+          </select>
+        </div>
+      </div>
 
-            <div class="w-full rounded rounded-lg bg-[#C8D7E0] p-4 mt-8 overflow-x-auto shadow-none shadow-[#18191A]"
-                 :class="{ 'shadow-md bg-[#1F2126]': !light_theme }">
-                <Tutorial/>
-            </div>
+      <div class="w-full rounded rounded-lg bg-[#C8D7E0] p-4 mt-8 overflow-x-auto shadow-none shadow-[#18191A]" :class="{ 'shadow-md bg-[#1F2126]': !light_theme }">
+        <Tutorial />
+      </div>
 
             <div class="w-11/12 mt-8 mx-auto rounded-lg">
                 <div class="rounded rounded bg-[#C5C5C5] p-2" :class="{ 'bg-[#4E5DE0]': !light_theme }">
